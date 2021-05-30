50c2e0f508aba6e109e89f01151096dcd89fd526
Mayank Jha <78186044+mayankjha-purdue@users.noreply.github.com>
2021-05-30 05:17:51 -0400

    Update test_flaskr.py

--- test_flaskr.py
+++ test_flaskr.py
@@ -228,7 +228,7 @@ def test_get_recent_given_model():
     # }
 
     headers = {'Content-Type': 'application/json'}
-    with patch('assignment2.sqlite3') as mocksql:
+    with patch('answer.sqlite3') as mocksql:
         mocksql.connect().cursor().fetchall.return_value = [(1622203201, 'bert-tiny', 'bully Leigh-Ann Galloway',
                                                              'who did holly matthews play in waterloo rd?',
                                                              "She attended the British drama school East 15 in 2005,and left after winning a high-profile role in the BBC drama Waterloo Road, playing the bully Leigh-Ann Galloway.[6] Since that role, Matthews has continued to act in BBC's Doctors, playing Connie Whitfield; in ITV's The Bill playing drug addict Josie Clarke; and she was back in the BBC soap Doctors in 2009, playing Tansy Flack.")]
